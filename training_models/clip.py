3505d82ab54c4c1723c5d5ceca34a1597032410f
MarcusYearwood <mxyearwood@gmail.com>
2025-03-07 07:12:06 -0500

    add ensemble attack

--- training_models/clip.py
+++ training_models/clip.py
@@ -1,3 +1,4 @@
+"""
 import torch
 from transformers import CLIPModel, CLIPTokenizer, CLIPVisionModel, CLIPVisionModelWithProjection, CLIPTextModelWithProjection, AutoTokenizer
 from PIL import Image
@@ -82,3 +83,80 @@ class MyClip():
         inputs.to(self.device)
         outputs = self.clip_text_model(**inputs)
         return outputs.text_embeds
+"""
+
+import torchvision
+from PIL import Image
+import torch
+import numpy as np
+
+import sys
+import os
+sys.path.append(os.path.abspath(".."))  # Adjust as needed
+
+from models.CLIP.clip import clip
+
+import warnings
+
+# models = [clip_vit_b_32]
+
+# final_preprocess = torchvision.transforms.Compose(
+#     [
+#         torchvision.transforms.Resize(clip_vit_b_32.visual.input_resolution, interpolation=torchvision.transforms.InterpolationMode.BICUBIC, antialias=True),
+#         # torchvision.transforms.Lambda(lambda img: torch.clamp(img, 0.0, 255.0) / 255.0),
+#         torchvision.transforms.CenterCrop(clip_vit_b_32.visual.input_resolution),
+#     ]
+# )
+
+class MyClipEnsemble():
+    def __init__(self, tau=2):
+        warnings.filterwarnings("ignore")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        clip_rn_50,_ = clip.load('RN50', device=self.device)
+        clip_rn_101,_ = clip.load('RN101', device=self.device)
+        clip_vit_b_16,_ = clip.load('ViT-B/16', device=self.device)
+        clip_vit_b_32,_ = clip.load('ViT-B/32', device=self.device)
+        clip_vit_l_14,_ = clip.load('ViT-L/14', device=self.device)
+        self.models = [clip_rn_50, clip_rn_101, clip_vit_b_16, clip_vit_b_32, clip_vit_l_14]
+
+        self.clip_preprocess = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.Resize(clip_vit_b_32.visual.input_resolution, interpolation=torchvision.transforms.InterpolationMode.BICUBIC, antialias=True),
+                # torchvision.transforms.Lambda(lambda img: torch.clamp(img, 0.0, 255.0) / 255.0),
+                torchvision.transforms.CenterCrop(clip_vit_b_32.visual.input_resolution),
+                torchvision.transforms.Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)), # CLIP imgs mean and std.
+            ]
+        )
+
+        self.costs = torch.ones(2, len(self.models))
+        self.tau = tau
+        
+    def encode_image(self, image, use_grad=True):
+        image_features_list = []
+        image_tgt = self.clip_preprocess(image)
+
+        context = torch.enable_grad() if use_grad else torch.no_grad()
+        with context:
+            for clip_model in self.models:
+                image_features = clip_model.encode_image(image_tgt)  # [bs, 512]
+                image_features = image_features / image_features.norm(dim=1, keepdim=True)
+                image_features_list.append(image_features)
+
+        return image_features_list
+    
+    def get_gradients(self, adv_image_features_list, tgt_image_features_list, adv_tensor):
+        model_losses=torch.zeros(len(self.models))
+        loss = torch.zeros(1).to(self.device)
+        for model_i, (pred_i, target_i) in enumerate(zip(adv_image_features_list, tgt_image_features_list)):
+            model_losses[model_i] = torch.mean(torch.sum(pred_i * target_i, dim=1))   
+
+        exp_cost_ratio = torch.exp(self.tau*(self.costs[1] / self.costs[0]+1e-16))
+        weights = torch.sum(exp_cost_ratio, dim=0) / (len(self.models)*exp_cost_ratio)
+        loss = torch.sum(weights * model_losses)
+
+        self.costs[1] = self.costs[0]
+        self.costs[0] = model_losses.clone().detach()
+
+
+        gradient = torch.autograd.grad(loss, adv_tensor)[0]
+        return gradient, torch.mean(model_losses)
